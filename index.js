f3a8b7201fe2191f6003f6b875760b2c6d7c05d8
Pranav Shinde <95024774+Pranavshinde678@users.noreply.github.com>
2025-07-08 23:50:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,24 @@ document.addEventListener("DOMContentLoaded", function() {
   typePhrase();
 });
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            observer.unobserve(entry.target); // remove if animation only once
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    document.querySelectorAll(".fade-in").forEach(el => {
+      observer.observe(el);
+    });
+  });
+
